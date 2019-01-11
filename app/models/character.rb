23e4df609651f28ca216ec920f602b8f472d947a
Joe Ganobsik <jganobsik@gmail.com>
2019-01-11 16:20:05 +0000

    Automatically backed up by Learn

--- app/models/character.rb
+++ app/models/character.rb
@@ -2,8 +2,9 @@ class Character < ActiveRecord::Base
   has_one :actors 
   has_one :shows
   
-  def Character.actor 
-    self.actors
+  def self.actor=(actor) 
+    actor = Actors.find_or_create_by(name: actor.to_s)
+    self.actors = actor
   end
   
   
