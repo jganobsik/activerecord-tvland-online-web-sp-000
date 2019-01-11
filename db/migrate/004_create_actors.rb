da88fcb920b5b8f805538928f52787d219e867a8
Joe Ganobsik <jganobsik@gmail.com>
2019-01-11 15:59:12 +0000

    Automatically backed up by Learn

---
+++ db/migrate/004_create_actors.rb
@@ -0,0 +1,9 @@
+class CreateShows < ActiveRecord::Migration[5.1]
+  def change
+    create_table :actors do |t|
+      #primary key of :id is created for us!
+      t.string :name
+      t.integer :show_id
+    end
+  end
+end
