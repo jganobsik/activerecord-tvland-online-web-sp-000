f5616816fefabe64fe7376847d6c616a5028a8dd
Joe Ganobsik <jganobsik@gmail.com>
2019-01-11 16:34:01 +0000

    Automatically backed up by Learn

--- db/migrate/008_add_actor_id_to_characters.rb
+++ db/migrate/008_add_actor_id_to_characters.rb
@@ -1,4 +1,4 @@
-class AddActorIdToCharacter < ActiveRecord::Migration[5.1]
+class AddActorIdToCharacters < ActiveRecord::Migration[5.1]
   #to make an additive change to a schema we create
   #a new migration, and then in the change method,
   #we make the change.
