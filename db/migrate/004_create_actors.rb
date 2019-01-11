bdc949196935ba73a81dffd34b9c9f17b0edf644
Joe Ganobsik <jganobsik@gmail.com>
2019-01-11 16:00:09 +0000

    Automatically backed up by Learn

--- db/migrate/004_create_actors.rb
+++ db/migrate/004_create_actors.rb
@@ -2,8 +2,8 @@ class CreateActors < ActiveRecord::Migration[5.1]
   def change
     create_table :actors do |t|
       #primary key of :id is created for us!
-      t.string :name
-      t.integer :show_id
+      t.string :first_name
+      t.string :last_name
     end
   end
 end
